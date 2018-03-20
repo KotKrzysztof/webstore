85a622f6f57d6a1cc7b1df98972307029429b2be
Krzysiek Kot <Krzysiek Kot@DESKTOP-QRC6OA6>
2018-03-20 09:28:04 +0100

    Add ContentViewResolver to show json and xml format

--- webstore/src/main/java/com/packt/webstore/domain/Product.java
+++ webstore/src/main/java/com/packt/webstore/domain/Product.java
@@ -2,36 +2,47 @@ package com.packt.webstore.domain;
 
 import java.math.BigDecimal;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.packt.webstore.validator.ProductId;
 
+@XmlRootElement 
 public class Product {
-	
-	@Pattern(regexp="[0-9]+", message="{Pattern.Product.productId.validation}")
-	@ProductId
+
+	@Pattern(regexp="P[1-9]+", message="{Pattern.Product.productId.validation}")
+	@ProductId 
 	private String productId;
-	@Size(min=4, max=50,message="{Size.Product.name.validation}")
+	
+	@Size(min=4, max=50, message="{Size.Product.name.validation}")
 	private String name;
-	@Min(value=0, message="{Min.Product.unitPrice.validation}")
+	
+	@Min(value=0, message="Min.Product.unitPrice.validation}")
 	@Digits(integer=8, fraction=2, message="{Digits.Product.unitPrice.validation}")
-	@NotNull(message="{NotNull.Product.unitPrice.validation}")
+	@NotNull(message= "{NotNull.Product.unitPrice.validation}")
 	private BigDecimal unitPrice;
 	private String description;
 	private String manufacturer;
 	private String category;
 	private long unitsInStock;
 	private long unitsInOrder;
-	private boolean discountnued;
+	private boolean discontinued;
 	private String condition;
-	private MultipartFile productImage;
-	
+	//@JsonIgnore 
+	private MultipartFile  productImage;
+
 	public Product() {
 		super();
-	}
-	
+}
+
 	public Product(String productId, String name, BigDecimal unitPrice) {
 		this.productId = productId;
 		this.name = name;
@@ -66,6 +77,15 @@ public class Product {
 		return description;
 	}
 
+	//@XmlTransient  
+	public MultipartFile getProductImage() {
+		return productImage;
+	}
+
+	public void setProductImage(MultipartFile productImage) {
+		this.productImage = productImage;
+	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
@@ -98,18 +118,18 @@ public class Product {
 		return unitsInOrder;
 	}
 
-	public void setUnitsInOrder(long unitsnOrder) {
-		this.unitsInOrder = unitsnOrder;
+	public void setUnitsInOrder(long unitsInOrder) {
+		this.unitsInOrder = unitsInOrder;
 	}
 
-	public boolean isDiscountnued() {
-		return discountnued;
+	public boolean isDiscontinued() {
+		return discontinued;
 	}
 
-	public void setDiscountnued(boolean discountnued) {
-		this.discountnued = discountnued;
+	public void setDiscontinued(boolean discontinued) {
+		this.discontinued = discontinued;
 	}
-
+	
 	public String getCondition() {
 		return condition;
 	}
@@ -117,43 +137,35 @@ public class Product {
 	public void setCondition(String condition) {
 		this.condition = condition;
 	}
-	
-	
-	public MultipartFile getProductImage() {
-		return productImage;
-	}
-
-	public void setProductImage(MultipartFile productImage) {
-		this.productImage = productImage;
-	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if(this == obj)
+		if (this == obj)
 			return true;
-		if(obj == null)
+		if (obj == null)
 			return false;
-		if(getClass() != obj.getClass())
+		if (getClass() != obj.getClass())
 			return false;
 		Product other = (Product) obj;
-		if(productId == null) {
-			if(other.productId != null)
+		if (productId == null) {
+			if (other.productId != null)
 				return false;
 		} else if (!productId.equals(other.productId))
 			return false;
 		return true;
-		}
-	
-	@Override 
+	}
+
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime*result + ((productId == null) ? 0 : productId.hashCode());
+		result = prime * result
+				+ ((productId == null) ? 0 : productId.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Product [productId= " + productId + ", nazwa=" + name +"]";
+		return "Produkt [productId=" + productId + ", nazwa=" + name +"]";
 	}
 }
